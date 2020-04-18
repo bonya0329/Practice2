7abc4e8eb8f9bac627dcc7bbcf2639af0ef20b54
bonya0329 <60532283+bonya0329@users.noreply.github.com>
2020-04-18 11:34:26 +0600

    pratice4

---
+++ WebApplication2/Data/MoviesContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models.Movies;
+
+namespace WebApplication2.Data
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(new Movie
+            {
+                Id = 1,
+                Author = "Todd Phillips",
+                Genre = "Crime , Drama , Thriller",
+                Name = "Joker",
+                Poster = "https://www.kinopoisk.ru/images/film_big/1048334.jpg",
+                CreatedAt = new DateTime(2019, 10, 3)
+            },
+            new Movie
+            {
+                Id = 2,
+                Author = "David Leitch",
+                Genre = "Action , Adventure",
+                Name = "Fast & Furious Presents: Hobbs & Shaw",
+                Poster = "https://m.media-amazon.com/images/I/A1nD1zDE9pL._SS500_.jpg",
+                CreatedAt = new DateTime(2019, 8, 2)
+            },
+            new Movie
+            {
+                Id = 3,
+                Author = "Jon Favreau",
+                Genre = "Adventure , Animation , Drama , Family , Musical",
+                Name = "The Lion King",
+                Poster = "https://images-na.ssl-images-amazon.com/images/I/91kXw97IUBL._AC_SL1500_.jpg",
+                CreatedAt = new DateTime(2019, 7, 19)
+            },
+            new Movie
+            {
+                Id = 4,
+                Author = "Joachim Rønning",
+                Genre = "Adventure , Family , Fantasy",
+                Name = "Maleficent: Mistress of Evil",
+                Poster = "https://st.kp.yandex.net/images/film_iphone/iphone360_916498.jpg",
+                CreatedAt = new DateTime(2019, 10, 18)
+            });
+        }
+    }
+}
--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebApplication2.Data;
 
 namespace WebApplication2
 {
@@ -16,6 +18,10 @@ namespace WebApplication2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MoviesContext>(options =>
+            {
+                options.UseSqlite("Filename=movies.db");
+            });
             services.AddMvc();
             services.AddMvc(options => options.EnableEndpointRouting = false);
         }
